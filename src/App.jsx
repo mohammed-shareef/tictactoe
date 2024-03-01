56433436708c9a5ac45a9a38d5a9c1dd69e6b0b2
Mohammed Shareef <>
2024-03-01 18:15:41 +0000

    Moved the game turn logic to the App component as it can be used by the log component as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 function App() {
 
   const [activePlayer,setActivePlayer] =  useState('X');
+  const [gameTurns,setGameTurns] = useState([]);
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex,colIndex){
     setActivePlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X' );
+
+    setGameTurns(prevTurns => {
+
+      let currentActivePlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player == 'X'){
+        currentActivePlayer = 'O';
+      }
+
+      const updatedTurn = [{square : {'row' : rowIndex,'col' : colIndex}, 
+                'player' : currentActivePlayer},...prevTurns];
+
+      return updatedTurn;
+    });
   }
 
   return (
@@ -17,9 +33,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayer={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
