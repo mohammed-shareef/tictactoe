f942057fcf41f2f6389a4a0c448ab4ac25c3699a
Mohammed Shareef <>
2024-03-01 15:49:14 +0000

    Highlight the currently active player and display the current player's symbol in the box

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,16 +6,18 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
     
   const [gameBoard,setGameBoard] =  useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex,colIndex){
      setGameBoard(previousGameBoard=>{
         const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
-        updatedBoard[rowIndex][colIndex] = 'X';
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
         return updatedBoard;
      });
+
+     onSelectSquare();
   }
 
   return (
